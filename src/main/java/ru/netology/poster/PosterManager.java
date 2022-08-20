ae1f32540a19d4327bfc8ad5cc9bd45ac8fe418f
Elena Levina <qa-levina@yandex.ru>
2022-08-20 12:49:12 +0300

    task completed

---
+++ src/main/java/ru/netology/poster/PosterManager.java
@@ -0,0 +1,82 @@
+package ru.netology.poster;
+
+public class PosterManager {
+    private String[] movies = new String[0];
+    private int limit = 10;
+
+    private int idMovie;
+    private String nameMovie;
+
+
+    public PosterManager() {
+
+    }
+
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+
+    //Добавление нового фильма.
+
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+
+    }
+
+    public String[] getMovies() {
+        return movies;
+    }
+
+    //Вывод всех фильмов в порядке добавления (findAll)
+
+    public String[] findAll() {
+        String[] tmp = new String[movies.length];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        tmp = movies;
+        return movies;
+
+
+    }
+
+    //Вывод максимум лимит* последних добавленных фильмов в обратном от добавления порядке (findLast)
+
+    public String[] findLast(int limit) {
+        int resultLenght;
+        if (limit <= movies.length) {
+            resultLenght = limit;
+        } else {
+            resultLenght = movies.length;
+        }
+        String[] tmp = new String[resultLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
+
+    }
+
+    public String[] findLastNoLimit() {
+        int resultLenght;
+        if (limit <= movies.length) {
+            resultLenght = limit;
+        } else {
+            resultLenght = movies.length;
+        }
+        String[] tmp = new String[resultLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
+
+    }
+}
+
+
---
+++ src/test/java/ru/netology/poster/PosterManagerTest.java
@@ -0,0 +1,121 @@
+package ru.netology.poster;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+    PosterManager manager = new PosterManager();
+    private String movie1;
+    private String movie2;
+    private String movie3;
+
+
+    @Test
+    public void addMovie() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie1, movie2, movie3};
+        String[] actual = manager.getMovies();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void findAll() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+
+        String[] expected = {movie3, movie2, movie1};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void findLast() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie3, movie2, movie1};
+        String[] actual = manager.findLast(5);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLastNoLimit() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie3, movie2, movie1};
+        String[] actual = manager.findLastNoLimit();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void findLastLessLimit() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie3, movie2};
+        String[] actual = manager.findLast(2);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLastOverLimit() {
+        PosterManager manager = new PosterManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie3, movie2, movie1};
+        String[] actual = manager.findLast(5);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLastLimit() {
+        PosterManager manager = new PosterManager(6);
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+
+        String[] expected = {movie3, movie2, movie1};
+        String[] actual = manager.findLastNoLimit();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+}
+
+
+
+
+
+
